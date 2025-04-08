740556aedd697a91026bb090511223c5d0d8e4dd
LeftStickDrift <arbert.o@hotmail.com>
2025-04-07 22:19:20 -0400

    fixed issues with test_end_to_end and test_user_registration

--- tests/test_user_registration.py
+++ tests/test_user_registration.py
@@ -1,4 +1,13 @@
-def test_user_registration():
+import pytest
+from Server import Server
+
+@pytest.fixture
+def server():
+    return Server()
+
+
+def test_user_registration(server):
+
     request_body = {"email": "newuser@example.com", "password": "password123", "privilege": "customer"}
     response = server.request_handler.request_registration(request_body)
     assert response["status_code"] == 200, "Registration should return status 200"
